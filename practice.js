45e07914cd773c3287aaceadd6e6ef74369f41a5
Nurul Islam <nurulbdpersonal@gmail.com>
2022-08-30 18:56:39 +0600

    first commit

---
+++ practice.js
@@ -0,0 +1,36 @@
+/*
+Module 33.5 Recap and Even more API Examples
+Which are learned from 30 to 33 module
+01.	Arrow function 
+02.	map(), forEach(), filter(), find() 
+03.	template string ``
+04.	API – fetch
+05.	DOM manipulation (recap) 
+
+
+Practice – 01
+Go to JSON Placeholder: https://jsonplaceholder.typicode.com/ 
+01.	Load comments and display comments on the UI
+02.	Display 20 comments only (first 20)
+03.	Add a click handler on the comment:
+04.	 if you click on the comment, it will open the relevant POST details: https://jsonplaceholder.typicode.com/posts/${postId} 
+
+Practice – 02
+01.	Load data for a random user 
+02.	Display image 
+03.	city, street,
+
+Practice – 03
+01.	 Practice MealDB again 
+
+Practice – 04
+01.	Explore sportsDB 
+02.	display something 
+03.	dynamic url API 
+04.	Add an event handler to display something by loading data
+
+Practice – 05
+01.	explore network tab 
+02.	display preview 
+
+*/
